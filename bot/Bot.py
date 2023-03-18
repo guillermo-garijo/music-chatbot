feffd02016bfed12f107751058c68c9c61b433d2
Victor Zalabardo Fernandez <v.zalabardo@gmail.com>
2023-03-18 23:42:45 +0100

    Say how you feel and get a song

--- bot/Bot.py
+++ bot/Bot.py
@@ -2,6 +2,7 @@ import nltk
 import random
 import User
 import ReadData
+import Utils
 import pandas as pd
 import sys
 from time import sleep
@@ -13,21 +14,27 @@ def print_hi(name):
 
 
 greetings = ['Hello!', 'Whatz up?', 'Nice to meet you!', 'You look handsome!']
-youWelcome = ['You Welcome! How can I help you?', 'My pleasure! Do you need something?','Happy to be of service. What do you want? :)', 'It was nothing. Ask me what you need :)']
+youWelcome = ['You Welcome! How are you today?', 'My pleasure! Do you need something? You can ask about artists, genres, top songs...','Happy to be of service. What do you want? :)', 'It was nothing. Ask me what you need :)']
 answerHowAreYou = ["I'm fine thanks! Do you need something?","I'm listening to music right now, but i'm here to help you, How can I help?"]
 genres = ['Hardcore', 'Techno', 'Rock']
 artists = ['Nerve Agent', 'Arctic Monkeys', 'Sefa']
 songs = ['Hoy estoy feliz - Nerve Agent', 'Cigarette Smoker Fiona - Arctic Monkeys', 'Going Under - Sefa']
 
+lastKey = ['hello']
 inputKeys = ["hello",
              "hi",
              "hey",
 
              ##Podriamos recomendar una cancion ejemplo: If you are fine/bad maybe you like this song! XXXXXXXXXXXX
-             "i'm Fine",
-             "I'm Good",
-             "I'm Bad",
-             "I'm Sad",
+             "i'm fine",
+             "i'm good",
+             "i'm happy",
+             "i feel good today",
+
+             "i'm not fine"
+             "i'm bad",
+             "i'm sad",
+             "i feel sad today",
 
              "how are you",
              "How's it going?",
@@ -44,7 +51,13 @@ inputKeys = ["hello",
              "show top songs",
              "show top music",
              "tell me top songs",
-             "tell me top music"]
+             "tell me top music",
+
+             "can you repeat"
+             "repeat please",
+             "repeat",
+             "again",
+             "give another one"]
 
 def printUI():
     print('\033[95m',"Hello I'm music-chatbot! Ask me anything you want about music.", '\033[0m')
@@ -52,19 +65,22 @@ def printUI():
 
 def start():
     user = User.User()
+    last_key = 'Hello'
     run = True
     while (run):
         print('\nUser > ', end='')
         user_input = input()
         if (user_input == 'exit'):
             run = False
-        ##print('\033[95m','Melody > ' + generateResponse(user_input, user),'\033[0m')
+
+        ##Typing Effect##
         print('\033[95mMelody > ', end='')
-        for char in generateResponse(user_input, user):
+        for char in generateResponse(user_input, user, lastKey):
             sleep(0.05)
             sys.stdout.write(char)
             sys.stdout.flush()
         print("\033[0m", end= '')
+        print()
 
 
 def main():
@@ -80,11 +96,10 @@ def main():
 processInput: Generate a response given an input
 ################################################################
 """
-def generateResponse(s, user):
+def generateResponse(s, user, lastKey):
     # hacer las reglas y generar la respuesta
     global genre, artist, year
     questionKey = processInput(s)
-
     ## USER IS SAYING HELLO ##
     if (questionKey == 'hello' or questionKey == 'hi' or questionKey == 'hey'):
         return greetings[random.randint(0, len(greetings) - 1)]
@@ -94,6 +109,14 @@ def generateResponse(s, user):
     ## USER IS SAYING THANKS ##
     if (questionKey == 'thanks' or questionKey == 'thank you'):
         return youWelcome[random.randint(0, len(youWelcome) - 1)]
+    ## USER IS HAPPY ##
+    if (questionKey =="i'm fine" or questionKey =="i'm good" or questionKey == "i'm happy" or questionKey == 'i feel good today'):
+        lastKey = lastKey
+        return getRandomHappySong()
+    ## USER IS SAD ##
+    if(questionKey =="i'm bad" or questionKey == "i'm sad" or questionKey == "i feel sad today" or questionKey == "i'm not fine"):
+        lastKey = lastKey
+        return getRandomSadSong()
 
     ## USER ASKING FOR BOT FAVOURITE GENRES/ARTISTS/SONGS
     if (questionKey == "what are your favourite" or questionKey == "which you like most" or questionKey == "your best" or questionKey == "can you tell your"):
@@ -156,16 +179,25 @@ def generateResponse(s, user):
 
         askedGenre= getGenre(s) ##Check if asking for genre
         askedArtist = getArtist(s) ##Check if asking for artist
+        askedNumber = getNumber(s)  # Get number asking
 
         if askedGenre:
-            askedNumber = getNumber(s) #Get number of genres asking
-            r = getXtopSongsGenre(askedNumber,askedGenre)
-            return r
-        if askedArtist:
-            askedNumber = getNumber(s) #Get number of genres asking
-            r = getXtopSongsArtist(askedNumber,askedArtist)
-            return r
+            return getXtopSongsGenre(askedNumber,askedGenre)
 
+        if askedArtist:
+            return getXtopSongsArtist(askedNumber,askedArtist)
+
+        if askedGenre and askedArtist:
+            return "At the moment I am not able to tell you the songs of an artist by genre"
+        else:
+            return getXTopSongs(askedNumber)
+
+    if (questionKey == "can you repeat" or questionKey == "repeat please" or questionKey =="repeat" or questionKey =="again" or questionKey =="give another one"):
+        if(lastKey =="i'm fine" or lastKey =="i'm good" or lastKey == "i'm happy" or lastKey == 'i feel good today'):
+            return getRandomHappySong()
+
+        if (lastKey == "i'm bad" or lastKey == "i'm sad" or lastKey == "i feel sad today" or lastKey == "i'm not fine"):
+            return getRandomHappySong()
     return "No entendi la pregunta"
 
 
@@ -175,7 +207,7 @@ processInput: Receives an input and return the most similar question key from in
 ################################################################
 """
 def processInput(input):
-    questionKey = process.extractOne(input, inputKeys)
+    questionKey = process.extractOne(input.lower(), inputKeys)
     ##print("Debug:",questionKey)
     if questionKey[1] >= 50:  # Consideramos que a partir de ratio 60, ha acertado
         return questionKey[0]  # Devolvemos clave, es decir, la pregunta predeterminada identificada
@@ -197,7 +229,7 @@ def getXtopSongsGenre(askedNumber,askedGenre):
         r = ('Here are top ' + str(askedNumber) + ' ' + askedGenre.capitalize() + ' songs: ')
         number = askedNumber
 
-    arrayData = pd.DataFrame(ReadData.getByParameter(['artist', 'title', 'the genre of the track']))  # {[a1, t1, g1], [a2, t2, g2], [a3, t3, g3]}
+    arrayData = pd.DataFrame(ReadData.getByParameter(['artist', 'title', 'the genre of the track','Popularity']))  # {[a1, t1, g1], [a2, t2, g2], [a3, t3, g3]}
     index = 0
     selected_songs_indexs = []
     while len(selected_songs_indexs) < number:
@@ -222,7 +254,7 @@ def getXtopSongsArtist(askedNumber,askedArtist):
         r = ('Here are top ' + str(askedNumber) + 'songs of ' + askedArtist.capitalize() + ":")
         number = askedNumber
 
-    arrayData = pd.DataFrame(ReadData.getByParameter(['artist', 'title', 'the genre of the track']))  # {[a1, t1, g1], [a2, t2, g2], [a3, t3, g3]}
+    arrayData = pd.DataFrame(ReadData.getByParameter(['artist', 'title', 'the genre of the track','Popularity']))  # {[a1, t1, g1], [a2, t2, g2], [a3, t3, g3]}
     index = 0
     selected_songs_indexs = []
     while len(selected_songs_indexs) < number:
@@ -233,6 +265,53 @@ def getXtopSongsArtist(askedNumber,askedArtist):
         index += 1
     return r
 
+def getRandomHappySong():
+    r = ("Maybe this song goes according to your mood: ")
+    arrayData = pd.DataFrame(ReadData.getByParameter(['artist', 'title', 'Valence','Popularity']))  # {[a1, t1, g1], [a2, t2, g2], [a3, t3, g3]}
+    index = random.randint(0,round(len(arrayData.values[0])/2))
+    found = False
+    while not found:
+        if arrayData[index][2] > 80:  ##comprobamos el valor de Valence (CUANTO MAS ALTO MAS HAPPY)
+            r += '\n               1.\033[94m' + arrayData[index][1] + ' - ' + arrayData[index][0] + '\033[0m'
+            found = True
+        index += 1
+    return r
+
+def getRandomSadSong():
+    r = ("Sometimes is good to be sad, try to listen this song for your mood: ")
+    arrayData = pd.DataFrame(ReadData.getByParameter(['artist', 'title', 'Valence','Popularity']))  # {[a1, t1, g1], [a2, t2, g2], [a3, t3, g3]}
+    index = random.randint(0,round(len(arrayData.values[0])/2))
+    found = False
+    while not found:
+        if arrayData[index][2] < 22:  ##comprobamos el valor de Valence (CUANTO MAS ALTO MAS HAPPY)
+            r += '\n               1.\033[94m' + arrayData[index][1] + ' - ' + arrayData[index][0] + '\033[0m'
+            found = True
+        index += 1
+    return r
+
+
+"""
+################################################################
+getXtopSongsGenre: Receive the wanted number of songs and a genre 
+                   and returns top X songs with that genre
+################################################################
+"""
+def getXTopSongs(askedNumber):
+    if(askedNumber == 0):
+        r = ('Here are top 3 songs: ')
+        number = 3
+    else:
+        r = ('Here are top ' + str(askedNumber) + ' songs: ')
+        number = askedNumber
+
+    arrayData = pd.DataFrame(ReadData.getByParameter(['artist', 'title', 'the genre of the track','Popularity']))  # {[a1, t1, g1], [a2, t2, g2], [a3, t3, g3]}
+    index = 0
+    selected_songs_indexs = []
+    while len(selected_songs_indexs) < number:
+        selected_songs_indexs.append(index)  ##añadimos a la lista
+        r += '\n              ' + str(len(selected_songs_indexs)) + '.\033[94m' + arrayData[index][1] + ' - ' + arrayData[index][0] + '\033[0m'
+        index += 1
+    return r
 """
 ################################################################
 getGenre: Receive an input and returns the genre that is there.
